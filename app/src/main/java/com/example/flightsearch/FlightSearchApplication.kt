38fa5995e74e19a55978fe4d8a2347f4824e51c5
ismat27 <adegbiteismail27@gmail.com>
2023-12-25 07:29:58 +0100

    initiate data store

--- app/src/main/java/com/example/flightsearch/FlightSearchApplication.kt
+++ app/src/main/java/com/example/flightsearch/FlightSearchApplication.kt
@@ -1,6 +1,10 @@
 package com.example.flightsearch
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.flightsearch.data.AppDataContainer
 import com.example.flightsearch.data.IAppDataContainer
 
@@ -10,6 +14,10 @@ class FlightSearchApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        container = AppDataContainer(this)
+        container = AppDataContainer(this, datastore)
     }
 }
+
+private const val APP_PREFERENCE_NAME = "APP_PREFERENCES"
+
+private val Context.datastore: DataStore<Preferences> by preferencesDataStore(APP_PREFERENCE_NAME)
--- app/src/main/java/com/example/flightsearch/data/AppDataContainer.kt
+++ app/src/main/java/com/example/flightsearch/data/AppDataContainer.kt
@@ -1,15 +1,17 @@
 package com.example.flightsearch.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 
 interface IAppDataContainer {
     val airportRepository: IAirportRepository
     val favoriteRepository: IFavoriteRepository
 }
 
-class AppDataContainer(context: Context): IAppDataContainer {
+class AppDataContainer(context: Context, dataStore: DataStore<Preferences>): IAppDataContainer {
     override val airportRepository: IAirportRepository by lazy {
-        AirportRepository(AppDatabase.getDatabase(context).getAirportDao())
+        AirportRepository(AppDatabase.getDatabase(context).getAirportDao(), dataStore)
     }
     override val favoriteRepository: IFavoriteRepository by lazy {
         FavoriteRepository(AppDatabase.getDatabase(context).getFavoriteDao())
