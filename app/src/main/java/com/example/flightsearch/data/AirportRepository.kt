69716f6087167bf6ba4b8b649bdc64c46edbb05f
ismat27 <adegbiteismail27@gmail.com>
2023-12-25 07:31:10 +0100

    set up data store to fetch and update searched term

--- app/src/main/java/com/example/flightsearch/data/AirportRepository.kt
+++ app/src/main/java/com/example/flightsearch/data/AirportRepository.kt
@@ -1,13 +1,47 @@
 package com.example.flightsearch.data
 
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
-interface IAirportRepository{
+interface IAirportRepository {
     fun search(iata: String): Flow<List<Airport>>
+    val searchTerm: Flow<String>
+    suspend fun updateSearchTerm(searchTerm: String)
 }
 
-class AirportRepository(private val airportDao: AirportDao): IAirportRepository {
+class AirportRepository(
+    private val airportDao: AirportDao,
+    private val dataStore: DataStore<Preferences>
+) : IAirportRepository {
+
+    override val searchTerm: Flow<String> = dataStore.data.catch {
+        if (it is IOException) {
+            Log.d("AirportRepository:searchTerm", it.message ?: "error reading search term")
+            emit(emptyPreferences())
+        } else {
+            throw it
+        }
+    }.map { preferences -> preferences[SEARCH_TERM] ?: "" }
+
+    override suspend fun updateSearchTerm(searchTerm: String) {
+        dataStore.edit { preferences ->
+            preferences[SEARCH_TERM] = searchTerm
+        }
+    }
+
     override fun search(iata: String): Flow<List<Airport>> {
         return airportDao.searchForAirport(iata)
+    }
+
+    companion object {
+        private val SEARCH_TERM = stringPreferencesKey("search_term")
     }
 }
--- app/src/main/java/com/example/flightsearch/ui/viewModels/AirportViewModel.kt
+++ app/src/main/java/com/example/flightsearch/ui/viewModels/AirportViewModel.kt
@@ -1,22 +1,62 @@
 package com.example.flightsearch.ui.viewModels
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.flightsearch.data.Airport
 import com.example.flightsearch.data.IAirportRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class AirportViewModel(private val airportRepository: IAirportRepository) : ViewModel() {
 
-    val airportSearchResult: StateFlow<List<Airport>> = airportRepository.search("INT").map {
-        it
-    }.stateIn(
-        initialValue = emptyList(),
-        started = SharingStarted.WhileSubscribed(),
-        scope = viewModelScope
-    )
+    var searchTerm by mutableStateOf("")
+        private set
+
+    val homeUiState: StateFlow<HomeUiState> = airportRepository.searchTerm
+        .map {
+            val airports = if (it.isBlank()) {
+                emptyList()
+            } else {
+                getSearchedAirport(it)
+            }
+            HomeUiState(searchTerm = it, searchedAirports = airports)
+        }
+        .stateIn(
+            initialValue = HomeUiState(),
+            started = SharingStarted.WhileSubscribed(5_000),
+            scope = viewModelScope
+        )
+
+    private suspend fun getSearchedAirport(searchTerm: String): List<Airport> {
+        return airportRepository.search(searchTerm).filterNotNull().first()
+    }
+
+    fun updateSearchTerm(newSearchTerm: String) {
+        searchTerm = newSearchTerm
+        viewModelScope.launch {
+            airportRepository.updateSearchTerm(searchTerm)
+        }
+    }
+
+    init {
+        viewModelScope.launch {
+            val storedSearchTerm:String = airportRepository.searchTerm.first()
+            searchTerm = storedSearchTerm
+        }
+    }
 
 }
+
+data class HomeUiState(
+    val searchTerm: String = "",
+    val searchedAirports: List<Airport> = emptyList()
+)
