6d06af2fc523fb3b514e6cd30ebc4dd26d17abaa
ismat27 <adegbiteismail27@gmail.com>
2023-12-25 07:32:15 +0100

    set up data store to fetch and update searched term

--- app/src/main/java/com/example/flightsearch/ui/viewModels/AirportViewModel.kt
+++ app/src/main/java/com/example/flightsearch/ui/viewModels/AirportViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.flightsearch.ui.viewModels
 
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
